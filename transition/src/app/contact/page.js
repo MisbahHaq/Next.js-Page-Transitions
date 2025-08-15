d4fbeacbdd1cbdc34f35bef33e89391c094eb940
Misbah Haque <misbahmuneer2003@gmail.com>
2025-08-15 12:04:05 +0500

    Update

---
+++ transition/src/app/contact/page.js
@@ -0,0 +1,14 @@
+export default function Page() {
+    return (
+        <>
+
+            <div className="container">
+                <div className="page-header">
+                    <h1>
+                        Get in touch
+                    </h1>
+                </div>
+            </div>
+        </>
+    );
+}
