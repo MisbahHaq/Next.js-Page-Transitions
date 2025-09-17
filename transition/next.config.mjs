563561d06883160cb27125eb5b7216672f0068bf
Misbah Haque <misbahmuneer2003@gmail.com>
2025-09-17 16:53:58 +0500

    Update

--- transition/next.config.mjs
+++ transition/next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+module.exports = {
+    redirects: async () => [
+        {
+            source: '/old-route',
+            destination: '/new-route',
+            permanent: true,
+        },
+    ],
+};
